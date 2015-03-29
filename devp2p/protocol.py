a3e9d7f43b3572091793c7024c851873f0eb1e28
Heiko Heiko <heiko@heiko.org>
2015-03-29 19:04:48 +0200

    wire protocol rewrite

--- devp2p/protocol.py
+++ devp2p/protocol.py
@@ -1,316 +1,212 @@
 # https://github.com/ethereum/go-ethereum/wiki/Blockpool
-import sys
-import time
-import gevent
-import struct
-import json
-from crypto import sha3
-from gevent import Greenlet
-from utils import idec
-import serialization
+import rlp
+from rlp import sedes
+from multiplexer import Packet
 import slogging
-log = slogging.get_logger('protocol.p2p').warn
-
-# packetize
-
-header_length = 5
-
-
-def decode_packet_header(message):
-    header = message[:header_length]
-    payload_len, cmd_id = struct.unpack('>BL', header)
-    return payload_len, cmd_id
-
-
-def encode_packet(cmd_id, data):
-    payload = json.dumps(data)
-    header = struct.pack('>BL', len(payload), cmd_id)
-    assert len(header) == header_length
-    return header + payload
+log = slogging.get_logger('protocol.p2p')
 
 
 class BaseProtocol(object):
 
     """
-    Component which translates between
-        messages from the p2p wire
-        and services
+    A protocol is collection of commands.
 
-    Keeps necessary state for the peer
-        e.g. last ping, sent/received hashes, ...
+    For each command X the following methods are created at initialization:
+    -    packet = protocol.create_X(*args, **kargs)
+    -   protocol.send_X(*args, **kargs) is a shortcut for:
+            protocol._send_packet(protocol.create_X(*args, **kargs))
+    - protocol._receive_X(data)
 
 
+    on protocol.receive_packet, the packet is deserialized according to the command.structure
+        and the command.receive method called with a dict containing the received data
+
+    the default implementation of command.receive calls callbacks
+    which can be registered in a list which is available as:
+    protocol.receive_X_callbacks
     """
+    protocol_id = 0
     name = ''
-    cmd_map = {}  # cmd_name: cmd_id
+    version = 0
 
-    def __init__(self, peer, cmd_offset=0):
-        self.peer = peer
-        self.cmd_offset = cmd_offset
-        self.cmd_map = dict((k, v + cmd_offset) for k, v in self.cmd_map.items())
-        self.rev_cmd_map = dict((v, k) for k, v in self.cmd_map.items())
+    class command(object):
 
-    def handle_message(self, cmd_id, payload):
-        data = json.loads(payload)
-        cmd_name = 'receive_%s' % self.rev_cmd_map[cmd_id]
-        cmd = getattr(self, cmd_name)
-        cmd(data)
+        """
+        - set cmd_id
+        - define structure for rlp de/endcoding by sedes
+        optionally implement
+        - create
+        - receive
 
-    def stop(self):
-        "called when peer disconnects, use to cleanup"
-        pass
+        default receive implementation, call callbacks with (proto_instance, data_dict)
+        """
+        cmd_id = 0
+        structure = []  # [(arg_name, rlp.sedes.type), ...]
 
+        def create(self, proto, *args, **kargs):
+            "optionally implement create"
+            assert isinstance(proto, BaseProtocol)
+            return args or kargs
 
-class ETHProtocol(BaseProtocol):
-    name = 'eth'
-    cmd_map = dict(status=0)
-    status_sent = False
-    status_received = False
+        def receive(self, proto, data):
+            "optionally implement receive"
+            for cb in self.receive_callbacks:
+                cb(proto, data)
 
-    def send_status(self):
-        data = dict(head_number=1,
-                    eth_version=49)
-        packet = encode_packet(self.cmd_map['status'], data)
-        self.peer._send_packet(packet)
-        self.status_sent = True
+        # no need to redefine the following ##################################
 
-    def receive_status(self, data):
-        # tell peermanager about spoken protocols
-        if not self.status_sent:
-            self.send_status()
-        self.status_received = True
+        def __init__(self):
+            self.receive_callbacks = []
 
+        @classmethod
+        def encode_payload(cls, data):
+            if isinstance(data, dict):  # convert dict to ordered list
+                data = [data[x[0]] for x in cls.structure]
+            assert len(data) == len(cls.structure)
+            return rlp.encode(data, sedes=sedes.List([x[1] for x in cls.structure]))
 
-class SHHProtocol(BaseProtocol):
-    name = 'shh'
-    cmd_map = dict(gossip=0)
+        @classmethod
+        def decode_payload(cls, rlp_data):
+            data = rlp.decode(rlp_data, sedes=sedes.List([x[1] for x in cls.structure]))
+            # convert to dict
+            return dict((cls.structure[i][0], v) for i, v in enumerate(data))
 
-    def send_gossip(self, gossip=''):
-        data = dict(gossip=gossip)
-        packet = encode_packet(self.cmd_map['gossip'], data)
-        self.peer._send_packet(packet)
+        # end command base ###################################################
 
-    def receive_gossip(self, data):
-        pass
+    _send_packet = lambda packet: None  # override in implementation
 
+    def __init__(self):
+        self._setup()
 
-class ConnectionMonitor(Greenlet):
-    ping_interval = 1.
-    response_delay_threshold = 2.
-    max_samples = 10
+    def _setup(self):
+        assert callable(self._send_packet)
 
-    def __init__(self, p2pprotocol):
-        Greenlet.__init__(self)
-        self.p2pprotocol = p2pprotocol
-        self.samples = []
-        self.last_request = time.time()
-        self.last_response = time.time()
+        # collect commands
+        klasses = [k for k in self.__class__.__dict__.values()
+                   if isinstance(k, type) and issubclass(k, self.command) and k != self.command]
+        assert len(set(k.cmd_id for k in klasses)) == len(klasses)
 
-    def __repr__(self):
-        return '<ConnectionMonitor(r)>'
+        def create_methods(klass):
+            instance = klass()
 
-    def track_request(self):
-        self.last_request = time.time()
+            def receive(packet):
+                "decode rlp, create dict, call receive"
+                assert isinstance(packet, Packet)
+                instance.receive(proto=self, data=klass.decode_payload(packet.payload))
 
-    def track_response(self):
-        self.last_response = time.time()
-        dt = self.last_response - self.last_request
-        self.samples.append(dt)
-        if len(self.samples) > self.max_samples:
-            self.samples.pop(0)
+            def create(*args, **kargs):
+                "get data, rlp encode, return packet"
+                print 'create called', klass
+                res = instance.create(self, *args, **kargs)
+                payload = klass.encode_payload(res)
+                return Packet(self.protocol_id, klass.cmd_id, payload=payload)
 
-    @property
-    def last_response_elapsed(self):
-        return time.time() - self.last_response
+            def send(*args, **kargs):
+                "create and send packet"
+                packet = create(*args, **kargs)
+                self._send_packet(packet)
 
-    @property
-    def latency(self, num_samples=0):
-        if not self.samples:
-            return None
-        num_samples = min(num_samples or self.max_samples, len(self.samples))
-        return sum(self.samples[:num_samples]) / num_samples
+            return receive, create, send, instance.receive_callbacks
 
-    def _run(self):
-        log('p2p.peer.monitor.started', monitor=self)
-        while True:
-            log('p2p.peer.monitor.pinging', monitor=self)
-            self.p2pprotocol.send_ping()
-            gevent.sleep(self.ping_interval)
-            log('p2p.peer.monitor.latency', monitor=self, latency=self.latency)
-            if self.last_response_elapsed > self.response_delay_threshold:
-                log('p2p.peer.monitor.unresponsive_peer', monitor=self)
-                self.stop()
+        for klass in klasses:
+            receive, create, send, receive_callbacks = create_methods(klass)
+            setattr(self, '_receive_' + klass.__name__, receive)
+            setattr(self, 'receive_' + klass.__name__ + '_callbacks', receive_callbacks)
+            setattr(self, 'create_' + klass.__name__, create)
+            setattr(self, 'send_' + klass.__name__, send)
 
-    def stop(self):
-        self.kill()
+        self.cmd_by_id = dict((klass.cmd_id, klass.__name__) for klass in klasses)
+
+    def receive_packet(self, packet):
+        cmd_name = self.cmd_by_id[packet.cmd_id]
+        cmd = getattr(self, '_receive_' + cmd_name)
+        cmd(packet)
 
 
 class P2PProtocol(BaseProtocol):
+    protocol_id = 0
     name = 'p2p'
     version = 2
 
-    # IF CHANGED, DO: git tag 0.6.<ETHEREUM_PROTOCOL_VERSION>
-    CLIENT_VERSION = 'Ethereum(py)/%s/%s' % (sys.platform, '0.7.0')
-    # the node s Unique Identifier and is the 512-bit hash that serves to
-    # identify the node.
-    NODE_ID = sha3('test')  # set in config
-    NETWORK_ID = 0
+    def __init__(self, peer):
+        # required by BaseProtocol
+        self._send_packet = peer.send_packet
+        # required by P2PProtocol
+        self.config = peer.config
+        assert peer.capabilities
+        assert callable(peer.stop)
+        assert callable(peer.receive_hello)
+        self.peer = peer
+        BaseProtocol.__init__(self)
 
-    cmd_map = dict(hello=0, ping=1, pong=2, disconnect=3, getpeers=4, peers=5)
+    class ping(BaseProtocol.command):
+        cmd_id = 1
 
-    def __init__(self, peer, cmd_offset, is_inititator=False):
-        """
-        initiater sends initial hello
-        """
-        super(P2PProtocol, self).__init__(peer, cmd_offset)
-        self.is_inititator = is_inititator
-        self.hello_received = False
-        self.connection_monitor = ConnectionMonitor(self)
-        self._handshake()
+        def receive(self, proto, data):
+            proto.send_pong()
 
-    def stop(self):
-        self.connection_monitor.stop()
+    class pong(BaseProtocol.command):
+        cmd_id = 2
 
-    @property
-    def peermanager(self):
-        return self.peer.peermanager
-
-    @property
-    def config(self):
-        return self.peermanager.app.config
-
-    @property
-    def nodeid(self):
-        return self.config['p2p']['node_id']
-
-    def _handshake(self):
-        if self.is_inititator:
-            self.send_hello()
-
-    def _send_packet(self, cmd_name, data):
-        assert isinstance(data, list)
-        cmd_id = self.cmd_map[cmd_name] + self.cmd_offset
-        msg = serialization.Serializer.dump_packet([cmd_id] + data)
-        self.peer.send(msg)
-
-    def send_ping(self):
-        log('p2p.send_ping', peer=self.peer)
-        self._send_packet('ping', [])
-        self.connection_monitor.track_request()
-
-    def receive_ping(self, data):
-        log('p2p.receive_ping', peer=self.peer)
-        self.send_pong()
-
-    def send_pong(self):
-        log('p2p.send_pong', peer=self.peer)
-        self._send_packet('pong')
-
-    def receive_pong(self, data):
-        log('p2p.receive_pong', peer=self.peer)
-        self.connection_monitor.track_response()
-
-    def send_disconnect(self, reason=''):
-        log('p2p.send_disconnect', peer=self.peer, reason=reason)
-        data = []
-        if reason:
-            data.append(serialization.Serializer.disconnect_reasons_map[reason])  # FIXME
-        self._send_packet('disconnect', data)
-        self.peer.stop()
-
-    def receive_disconnect(self, data):
-        reason = serialization.Serializer.disconnect_reasons_map_by_id[idec(data[0])]
-        log('p2p.receive_disconnect', peer=self.peer, reason=reason)
-        self.peer.stop()
-
-    def send_getpeers(self):
-        return self._send_packet('getpeers')
-
-    def receive_getpeers(self):
-        self.send_peers()
-
-    def send_peers(self):
-        '''
-        :param peers: a sequence of (ip, port, pid)
-        :return: None if no peers
-        '''
-        data = []
-        for peer in self.peermanager.peers:
-            ip, port = peer.ip_port
-            assert ip.count('.') == 3
-            ip = ''.join(chr(int(x)) for x in ip.split('.'))
-            data.append([ip, port, peer.nodeid])
-        return self._send_packet('peers', data)
-
-    def receive_peers(self, data):
-        pass  # FIXME
-
-    def send_hello(self):
-        """
-        0x01 Hello: [0x01: P, protocolVersion: P, clientVersion: B, [cap0: B, cap1: B, ...]
-        listenPort: P, id: B_64]
-
-        protocolVersion: The underlying network protocol. 0
-        clientVersion: The underlying client. A user-readable string.
-        capN: A peer-network capability code, readable ASCII and 3 letters. Currently only "eth
-        and "shh" are known.
-        listenPort: The port on which the peer is listening for an incoming connection.
-        id: The identity and public key of the peer.
-        """
-        log('p2p.send_hello', peer=self.peer)
-        capabilities = [(p.name, p.version) for p in self.peer.protocols]
-
-        data = [
-            self.version,
-            self.CLIENT_VERSION,
-            capabilities,
-            self.config['p2p']['listen_port'],
-            self.nodeid
+    class hello(BaseProtocol.command):
+        cmd_id = 0
+        max_protocols = 64
+        _sedes_capabilites_tuple = sedes.List([sedes.binary, sedes.big_endian_int])
+        structure = [
+            ('version', sedes.big_endian_int),
+            ('client_version', sedes.big_endian_int),
+            ('capabilities', sedes.List([_sedes_capabilites_tuple] * max_protocols, strict=False)),
+            ('listen_port', sedes.big_endian_int),
+            ('nodeid', sedes.binary)
         ]
-        self._send_packet('hello', data)
 
-    def _recv_hello(self, data):
-        log('p2p.receive_hello', peer=self.peer)
-        # 0x01 Hello: [0x01: P, protocolVersion: P, clientVersion: B, [cap0: B,
-        # cap1: B, ...], listenPort: P, id: B_64]
-        _decode = (idec, str, list, idec, str)
-        try:
-            data = [_decode[i](x) for i, x in enumerate(data)]
-            network_protocol_version, client_version = data[0], data[1]
-            capabilities, listen_port, node_id = data[2], data[3], data[4]
-            self.capabilities = [(p, ord(v)) for p, v in capabilities]
-        except (IndexError, ValueError) as e:
-            log('could not decode hello', peer=self, error=e)
-            return self.send_Disconnect(reason='Incompatible network protocols')
+        def create(self, proto):
+            return dict(version=proto.version,
+                        client_version=proto.config['version'],
+                        capabilities=proto.peer.capabilities,
+                        listen_port=proto.config['p2p']['listen_port'],
+                        nodeid=proto.config['p2p']['nodeid'],
+                        )
 
-        assert node_id
-        if node_id == self.nodeid:
-            log.critical('connected myself')
-            return self.send_Disconnect(reason='Incompatible network protocols')
+        def receive(self, proto, data):
+            log.debug('receive_hello', peer=proto.peer, version=data['version'])
+            if data['nodeid'] == proto.config['p2p']['nodeid']:
+                log.debug('connected myself')
+                return proto.send_disconnect(
+                    reason=proto.disconnect.reason.incompatible_network_protocols)
+            if data['version'] != proto.version:
+                log.debug('incompatible network protocols', peer=proto.peer,
+                          expected=proto.version, received=data['version'])
+                return proto.send_disconnect(
+                    reason=proto.disconnect.reason.incompatible_network_protocols)
 
-        self.capabilities = [(p, ord(v)) for p, v in capabilities]
-        log('received hello',
-            peer=self,
-            network_protocol_version=network_protocol_version,
-            node_id=node_id.encode('hex'),
-            client_version=client_version,
-            capabilities=self.capabilities)
+            proto.peer.receive_hello(**data)
 
-        if network_protocol_version != self.version:
-            log('incompatible network protocols',
-                peer=self,
-                expected=self.version,
-                received=network_protocol_version)
-            return self.send_Disconnect(reason='Incompatible network protocols')
+    class disconnect(BaseProtocol.command):
+        cmd_id = 3
+        structure = [('reason', sedes.big_endian_int)]
 
-        self.hello_received = True
-        self.peer.client_version = client_version
-        self.peer.nodeid = node_id
-        self.peer.listen_port = listen_port  # replace connection port with listen port
+        class reason(object):
+            disconnect_requested = 0
+            tcp_sub_system_error = 1
+            bad_protocol = 2
+            useless_peer = 3
+            too_many_peers = 4
+            already_connected = 5
+            wrong_genesis_block = 6
+            incompatible_network_protocols = 7
+            client_quitting = 8
 
-        if not self.is_inititator:
-            self.send_hello()
+        def reason_name(self, id):
+            return [k for k, v in self.reason.__dict__.items() if v == id][0]
 
-        # tell peermanager about spoken protocols
-        self.peer.peermanager.on_hello_received(self, data)
-        self.connection_monitor.start()
+        def create(self, proto, reason=reason.client_quitting):
+            assert self.reason_name(reason)
+            log.debug('send_disconnect', peer=proto.peer, reason=self.reason_name(reason))
+            proto.peer.stop()
+            return dict(reason=reason)
+
+        def receive(self, proto, data):
+            log.debug('receive_disconnect', peer=proto.peer,
+                      reason=self.reason_name(data['reason']))
+            proto.peer.stop()
---
+++ devp2p/tests/test_p2pprotocol.py
@@ -0,0 +1,85 @@
+from devp2p.protocol import P2PProtocol
+
+# notify peer of successfulll handshake!
+# so other protocols get registered
+# so other protocols can do their handshake
+
+
+class PeerMock(object):
+    packets = []
+    config = dict(p2p=dict(listen_port=3000, nodeid='\x00' * 64), version=1)
+    capabilities = [('p2p', 2), ('eth', 57)]
+    stopped = False
+    hello_received = False
+
+    def receive_hello(self, version, client_version, capabilities, listen_port, nodeid):
+        for name, version in capabilities:
+            assert isinstance(name, str)
+            assert isinstance(version, int)
+        self.hello_received = True
+
+    def send_packet(self, packet):
+        print 'sending', packet
+        self.packets.append(packet)
+
+    def stop(self):
+        self.stopped = True
+
+
+def test_protocol():
+    peer = PeerMock()
+    proto = P2PProtocol(peer=peer)
+
+    # ping pong
+    proto.send_ping()
+    ping_packet = peer.packets.pop()
+    proto._receive_ping(ping_packet)
+    pong_packet = peer.packets.pop()
+    proto._receive_pong(pong_packet)
+    assert not peer.packets
+
+    # hello (fails same nodeid)
+    proto.send_hello()
+    hello_packet = peer.packets.pop()
+    proto._receive_hello(hello_packet)
+    disconnect_packet = peer.packets.pop()  # same nodeid
+    assert peer.stopped
+    peer.stopped = False
+
+    # hello (works)
+    proto.send_hello()
+    hello_packet = peer.packets.pop()
+    peer.config['p2p']['nodeid'] = '\x01' * 64  # change nodeid
+    proto._receive_hello(hello_packet)
+    assert not peer.packets
+    assert not peer.stopped
+    assert peer.hello_received
+
+    # disconnect
+    proto.send_disconnect(reason=proto.disconnect.reason.disconnect_requested)
+    disconnect_packet = peer.packets.pop()
+    proto._receive_disconnect(disconnect_packet)
+    assert not peer.packets
+    assert peer.stopped
+
+
+def test_callback():
+    peer = PeerMock()
+    proto = P2PProtocol(peer=peer)
+
+    # setup callback
+    r = []
+
+    def cb(data):
+        r.append(data)
+    proto.receive_pong_callbacks.append(cb)
+
+    # trigger
+    proto.send_ping()
+    ping_packet = peer.packets.pop()
+    proto._receive_ping(ping_packet)
+    pong_packet = peer.packets.pop()
+    proto._receive_pong(pong_packet)
+    assert not peer.packets
+    assert len(r) == 1
+    assert r[0] == dict()
