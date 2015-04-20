67805443fdd27155a1c2b9a9b246650cc6293317
Heiko Heiko <heiko@heiko.org>
2015-04-20 22:23:19 +0200

    spawn blocking queue/socket handlers

--- devp2p/peer.py
+++ devp2p/peer.py
@@ -37,6 +37,10 @@ class Peer(gevent.Greenlet):
         assert issubclass(self.peermanager.wire_protocol, P2PProtocol)
         self.connect_service(self.peermanager)
 
+        # assure, we don't get messages while replies are not read
+        self.safe_to_read = gevent.event.Event()
+        self.safe_to_read.set()
+
     @property
     def remote_pubkey(self):
         "if peer is responder, then the remote_pubkey will not be available"
@@ -151,48 +155,43 @@ class Peer(gevent.Greenlet):
         protocol.receive_packet(packet)
 
     def send(self, data):
-        if data:
-            try:
-                self.connection.sendall(data)  # check if gevent chunkes and switches contexts
-            except gevent.socket.error as e:
-                log.info('write error', errno=e.errno, reason=e.strerror)
-                self.report_error('write error')
-                if e.errno == 32:  # Broken pipe
-                    self.report_error('broken pipe')
-                    self.stop()
-                else:
-                    raise e
-
-    def _run(self):
-        """
-        Loop through queues
-
-        fixme: option to wait for any finished event
-             gevent.wait(objects=None, timeout=None, count=None)
-             and wrap connection.wait_ready in an event
-
-        mux.evt
-        and spawn a wait_read which triggers an event
-
-        """
-        while not self.is_stopped:
-            # handle decoded packets
-            while not self.mux.packet_queue.empty():
-                self._handle_packet(self.mux.get_packet())
-
-            # read egress data from the multiplexer queue
-            emsg = self.mux.get_message()
-            if emsg:
-                self.send(emsg)
-                timeout = 0
+        if not data:
+            return
+        self.safe_to_read.clear()  # make sure we don't accept any data until message is sent
+        try:
+            self.connection.sendall(data)  # check if gevent chunkes and switches contexts
+        except gevent.socket.error as e:
+            log.info('write error', errno=e.errno, reason=e.strerror)
+            self.report_error('write error')
+            if e.errno == 32:  # Broken pipe
+                self.report_error('broken pipe')
+                self.stop()
             else:
-                timeout = self.wait_read_timeout
+                raise e
+        except gevent.socket.timeout:
+            log.info('write timeout')
+            self.report_error('write timeout')
+            self.stop()
+        self.safe_to_read.set()
+
+    def _run_egress_message(self):
+        while not self.is_stopped:
+            self.send(self.mux.message_queue.get())
+
+    def _run_decoded_packets(self):
+        # handle decoded packets
+        while not self.is_stopped:
+            self._handle_packet(self.mux.packet_queue.get())  # get_packet blocks
+
+    def _run_ingress_message(self):
+        gevent.spawn(self._run_decoded_packets)
+        gevent.spawn(self._run_egress_message)
+
+        while not self.is_stopped:
+            self.safe_to_read.wait()
+            gevent.socket.wait_read(self.connection.fileno())
             try:
-                #log.debug('polling data', peer=self, timeout=timeout)
-                gevent.socket.wait_read(self.connection.fileno(), timeout=timeout)
                 imsg = self.connection.recv(4096)
-            except gevent.socket.timeout:
-                continue
             except gevent.socket.error as e:
                 log.info('read error', errno=e.errno, reason=e.strerror, peer=self)
                 self.report_error('network error %s' % e.strerror)
@@ -200,20 +199,20 @@ class Peer(gevent.Greenlet):
                     self.stop()
                 else:
                     raise e
-
+                    break
             if imsg:
                 try:
                     self.mux.add_message(imsg)
                 except rlpxcipher.RLPxSessionError as e:
-                    log.debug('rlpx serssion error', peer=self)
-                    self.report_error('rlpx serssion error')
+                    log.debug('rlpx session error', peer=self)
+                    self.report_error('rlpx session error')
                     self.stop()
-                    break
             else:
-                log.debug('loop_socket.not_data', peer=self)
+                log.debug('no data on socket', peer=self)
                 self.report_error('no data on socket')
                 self.stop()
-                break
+
+    _run = _run_ingress_message
 
     def stop(self):
         if not self.is_stopped:
